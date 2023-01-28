a5f95664fbecda227d5cbf46872c1ebfaaa161a1
Christophe Guillaumie <96783888+cguillaumie@users.noreply.github.com>
2023-01-28 12:54:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ passenger_count = st.select_slider("Select number of passenger", options=[1,2, 3
 GOOGLE_API_KEY = "AIzaSyCsUnUor-GuaCrn7xivVkOVwTSf0XtagTU"
                                    
 #https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=YOUR_API_KEY  
-GEOCODE_URL_pickup = "https://maps.googleapis.com/maps/api/directions/json?origin="pick_up_adress"&destination="dropoff_adress"&key="+GOOGLE_API_KEY
+GEOCODE_URL_pickup = "https://maps.googleapis.com/maps/api/directions/json?origin="pick_up_adress"&destination="dropoff_adress"&key="GOOGLE_API_KEY
                                    
 geo_response = requests.request("GET", GEOCODE_URL_pickup)
                                 
